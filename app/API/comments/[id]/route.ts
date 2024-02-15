5109a63a5f9a64654bab049dcf857d8490ddb374
Pavel Cof <cubalab@gmail.com>
2024-02-15 13:28:44 +0300

    API

--- app/API/comments/[id]/route.ts
+++ app/API/comments/[id]/route.ts
@@ -1,25 +1,38 @@
 import { comment } from "postcss"
 import {comments} from "../data"
 import { headers } from "next/headers"
+import { redirect } from "next/navigation"
 
 export async function GET(_request:Request,{params}:{params:{id:string}}) {
 
     const comment = comments.find((comment)=>comment.id===parseInt(params.id))
-
+ 
+    if(!comment){
+        redirect("/API/comments");
+    }
     return Response.json(comment)
 }
 export async function PATCH(request:Request,{params}:{params:{id:string}}) {
     const body = await request.json()
     const {text} = body
     const index = comments.findIndex((comment)=>comment.id===parseInt(params.id))
-    comments[index].text=text
+
+    if(index>0){
+        comments[index].text=text
+    }else{
+        redirect("/API/comments");
+    }
 
     return  Response.json( comments[index] )
 }
 export async function DELETE(_request:Request,{params}:{params:{id:string}}) {
 
     const index = comments.findIndex((comment)=>comment.id===parseInt(params.id))
-    comments.splice(index,1)
+    if(index>0){
+        comments.splice(index,1)
+    }else{
+        redirect("/API/comments");
+    }
     
     return  Response.json({ comment:"Комментарий удалён"})
 }
--- app/API/comments/route.ts
+++ app/API/comments/route.ts
@@ -1,10 +1,21 @@
 import { comment } from "postcss"
 import {comments} from "./data"
 import { headers } from "next/headers"
+import { NextRequest } from "next/server"
+//запрос GET без параметров
+// export async function GET() {
+//     return Response.json(comments)
+// }
 
-export async function GET() {
-    return Response.json(comments)
+//запрос GET с параметрами (поиск сомментария с текстом из параметра query) comments/?query="text"
+export async function GET(request:NextRequest) {
+
+    const searchParams = request.nextUrl.searchParams
+    const query =searchParams.get("query")
+    const filteredComments = query ? comments.filter((comment)=>comment.text.includes(query)):comments
+    return Response.json(filteredComments)
 }
+
 export async function POST(request:Request) {
     const comment = await request.json()
     const newcomments ={
